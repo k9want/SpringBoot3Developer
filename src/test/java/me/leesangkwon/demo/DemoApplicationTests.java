e62a21b18944033c05780aa138364ce696c28440
k9want <zndwlrnjr26@daum.net>
2023-09-19 14:06:12 +0900

    :technologist: TestController - @RestController, @GetMapping의 역할

---
+++ src/test/java/me/leesangkwon/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.leesangkwon.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
