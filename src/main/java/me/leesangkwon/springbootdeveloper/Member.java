34d663adee981276a2397e2988346dd3805317c9
k9want <zndwlrnjr26@daum.net>
2023-09-20 00:37:14 +0900

    :technologist: Add MemberService, 데이터베이스 조작이 편해지는 ORM

--- src/main/java/me/leesangkwon/springbootdeveloper/Member.java
+++ src/main/java/me/leesangkwon/springbootdeveloper/Member.java
@@ -9,10 +9,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Getter
-@Entity
+@Entity // name 지정안하면 클래스 이름과 같은 테이블과 매핑
+//@Entity (name = "member_list") member_list라는 이름을 가진 테이블과 매핑
 public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id // Long타입의 id 필드를 테이블의 기본키로 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키 자동으로 1씩 증가
     @Column(name = "id", updatable = false)
     private Long id;
 
