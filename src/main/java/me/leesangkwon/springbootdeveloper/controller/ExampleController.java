54014e67936dce0127581c8b53873e5dea65c730
k9want <zndwlrnjr26@daum.net>
2023-09-21 13:02:12 +0900

    :technologist: Add ExampleController & example.html , thymeleaf 연습

---
+++ src/main/java/me/leesangkwon/springbootdeveloper/controller/ExampleController.java
@@ -0,0 +1,36 @@
+package me.leesangkwon.springbootdeveloper.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Controller
+public class ExampleController {
+
+    @GetMapping("/thymeleaf/example")
+    public String thymeleafExample(Model model) {
+        Person person = new Person();
+        person.setId(1L);
+        person.setName("thymeleaf A");
+        person.setAge(20);
+        person.setHobbies(List.of("운동", "독서"));
+
+        model.addAttribute("person", person);
+        model.addAttribute("today", LocalDate.now());
+        return "example";
+    }
+
+    @Setter
+    @Getter
+    class Person {
+        private Long id;
+        private String name;
+        private int age;
+        private List<String> hobbies;
+    }
+}
