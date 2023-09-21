9f7e92d6c8231261cb5cd9ee17112ae913c1772b
k9want <zndwlrnjr26@daum.net>
2023-09-21 16:41:51 +0900

    :technologist: 블로그 글 생성 구현

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -36,5 +36,23 @@ if (modifyBtn) {
                 location.replace(`/articles/${id}`)
             })
     })
-
 }
+
+const createBtn = document.getElementById("create-btn");
+
+createBtn.addEventListener('click', event => {
+
+    fetch(`/api/articles`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            title: document.getElementById('title').value,
+            content: document.getElementById('content').value
+        }),
+    }).then(() => {
+        alert('등록 완료되었습니다.');
+        location.replace(`/articles`)
+    })
+})
