7a62ffe9d3fed162d19591d3d23d364e66344bb3
k9want <zndwlrnjr26@daum.net>
2023-09-22 12:43:29 +0900

    :technologist: Spring Security로 로그인/로그아웃/ 회원 가입 구현

---
+++ src/main/java/me/leesangkwon/springbootdeveloper/dto/AddUserRequest.java
@@ -0,0 +1,11 @@
+package me.leesangkwon.springbootdeveloper.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddUserRequest {
+    private String email;
+    private String password;
+}
